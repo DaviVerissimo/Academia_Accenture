1826276a662964bfa6d20fe4ba3b056be4747c41
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-08-06 22:37:21 -0300

    Corrida JPA

---
+++ CorridaJPA/src/main/java/acc/br/CorridaJPA/CorridaJpaApplication.java
@@ -0,0 +1,13 @@
+package acc.br.CorridaJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorridaJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorridaJpaApplication.class, args);
+    }
+// http://localhost:8080/swagger-ui/index.html#/
+}
---
+++ CorridaJPA/src/main/java/acc/br/CorridaJPA/repository/CorridaRepository.java
@@ -0,0 +1,17 @@
+package acc.br.CorridaJPA.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import acc.br.CorridaJPA.model.Corrida;
+import jakarta.transaction.Transactional;
+
+@Repository
+public interface CorridaRepository extends JpaRepository<Corrida, Long> {
+	List<Corrida> findByLocalId(Long localId);
+
+	@Transactional
+	void deleteByLocalId(Long localId);
+}
